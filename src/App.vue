f31c8745d24ecc76c84ea925bf4ec4a7e14eceaf
maopineda <ingmauriciopineda@gmail.com>
2021-11-28 12:26:12 -0500

    Conexión a API con mensaje de bienvenida

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!--<img src="./assets/logo.png">
+    <HelloWorld/>-->
+    <Home/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+//import HelloWorld from './components/HelloWorld'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld,
+    Home
   }
 }
 </script>
@@ -19,10 +22,10 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  /*-webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;*/
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="div-home">
+        <div id="div-bienvenida">
+            <h1>Bienvenido a r-store</h1>
+            <p>{{ respuestaApi }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Home',
+    data: function() {
+        return {
+            respuestaApi: ""
+        }
+    },
+    methods: {
+        llamarAPI: function() {
+            axios
+            .get("http://localhost:8000")
+            .then(respuesta => {
+                this.respuestaApi = respuesta.data.mensaje;
+            })
+            .catch(error => {
+                this.respuestaApi = "No hay conexión con la API"
+            })
+        }
+    },
+    created: function() {
+        this.llamarAPI();
+    }
+}
+</script>
+
+
+<style scoped>
+#div-home {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#div-bienvenida {
+    text-align: center;
+}
+#div-bienvenida h1 {
+    color: red;
+}
+#div-bienvenida p {
+    color: green;
+}
+</style>
